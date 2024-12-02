63b98193cc401f0e94a89f557a555a8252fcf10e
Sam <samzhou@aslcn.com.cn>
2024-12-02 13:57:13 +0800

    feat: upload file

--- src/main/java/com/samzhou/controller/FileUploadController.java
+++ src/main/java/com/samzhou/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.samzhou.controller;
 
 import com.samzhou.pojo.Result;
+import com.samzhou.utils.AliOssUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,8 +18,9 @@ public class FileUploadController {
         String filename = file.getOriginalFilename();
         //保证文件的名字唯一
         String uuidFilename = UUID.randomUUID().toString() + filename.substring(filename.lastIndexOf("."));
-        file.transferTo(new File("D:\\data\\files\\" + uuidFilename));
-        return Result.success("图片url：" + uuidFilename);
+       // file.transferTo(new File("D:\\data\\files\\" + uuidFilename));
+        String url = AliOssUtil.uploadFile(uuidFilename,file.getInputStream());
+        return Result.success(url);
 
 
     }
